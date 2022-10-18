b2ac02d02bd691cb85160de9aaccc171daeab969
goldipl <mgodlewski92@gmail.com>
2022-10-18 21:24:55 +0200

    add star rating component

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,9 +1,30 @@
-import { FaStar } from 'react-icons/fa'
+import { SetStateAction, useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+import './../css/starrating.css';
 
 const StarRating = () => {
+    const [star, setStarValue] = useState(0);
+
     return ( 
         <div>
-            <FaStar />
+            {[...Array(5)].map((value, i) => {
+                const starValue: number = i + 1;
+                return (
+                <label>
+                    <input 
+                        type='radio' 
+                        name='rating' 
+                        value={starValue}
+                        onClick={() => setStarValue(starValue)}
+                    />
+                    <FaStar 
+                        className='star' 
+                        size={20}
+                        color={starValue <= star ? 'goldenrod' : 'grey' }
+                    />
+                </label>
+                );
+            })}
         </div>
      );
 }
